0889a4aef25e023e9fdeb38a64658396d373acd3
Quality Coder <paul@qualitycoder.net>
2019-08-27 22:38:14 -0500

    Adding projects and webhooks tables

---
+++ database/migrations/2019_08_28_032259_webhooks.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Webhooks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webhooks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->string('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webhooks');
+    }
+}
