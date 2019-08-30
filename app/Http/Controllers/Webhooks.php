45835a585a4c03bdcbf1bea08833f481dfff7dec
Quality Coder <paul@qualitycoder.net>
2019-08-29 22:01:18 -0500

    more error logging

--- app/Http/Controllers/Webhooks.php
+++ app/Http/Controllers/Webhooks.php
@@ -18,8 +18,9 @@ class Webhooks extends Controller
 
     public function store(Request $request) {
         $data = $request->all();
-        $data_str = implode('[[QC]]', $data);
         
+        $data_str = implode('[[QC]]', $data);
+
         error_log("***** {$data_str} *****");
 
         $response = $this->service->saveHook($data);
