0bf4b0c8adcd4947ee1a507647de836f29b40ec5
Quality Coder <paul@qualitycoder.net>
2019-08-29 22:23:44 -0500

    webhook test

--- app/Http/Controllers/Webhooks.php
+++ app/Http/Controllers/Webhooks.php
@@ -18,7 +18,8 @@ class Webhooks extends Controller
 
     public function store(Request $request) {
         $data = $request->json();
-        
+
+        /* comment */
         error_log(print_r($data, true));
 
         $response = $this->service->saveHook($data);
