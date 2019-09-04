e81b60fa337e25147dcbfb9a9647ed2d07d40876
Quality Coder <paul@qualitycoder.net>
2019-09-03 22:27:50 -0500

    test data

--- app/Services/Projects.php
+++ app/Services/Projects.php
@@ -24,7 +24,7 @@ class Projects
 
     public function createProject(array $data) {
         $data = $this->cleanseInput($data);
-
+return json_encode($data);
         $newProject = $this->projectMdl->newInstance($data);
         $newProject->save();
         return $newProject;
@@ -51,7 +51,7 @@ class Projects
     private function cleanseInput($data) {
 
         $columns = Schema::getColumnListing($this->projectMdl->getTable());
-
+return $columns;
         foreach($data as $field => $value) {
             if(in_array($field, $columns)){
                 $columns[$field] = $value;
