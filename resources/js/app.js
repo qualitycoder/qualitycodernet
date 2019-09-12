b3abdd337456e58f1d7e9d06800762699defe306
Quality Coder <paul@qualitycoder.net>
2019-09-12 00:57:34 -0500

    Vue stuffs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,45 +1,14 @@
-import ProjectDetail from "./components/ProjectDetail";
-import Projects from "./components/Projects";
-import Home from "./components/Home";
-import About from "./components/About";
+import './bootstrap';
+import Vue from 'vue';
+import Vuetify from "vuetify";
+import Routes from '@/js/routes.js';
+import App from '@/js/views/App';
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-window.VueRouter = require('vue-router');
-
-const files = require.context('./', true, /\.vue$/i);
-files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home.vue
-        },
-        {
-            path: '/about',
-            name: 'about',
-            component: About.vue,
-        },
-        {
-            path: '/projects',
-            name: 'projects',
-            component: Projects.vue,
-        },
-        {
-            path: '/projects/{id}',
-            name: 'projectDetail',
-            component: ProjectDetail.vue,
-        },
-    ],
-});
-
+Vue.use(Vuetify);
 const app = new Vue({
-    el: '#app',
-    router: router
+    el:'#app',
+    router: Routes,
+    render: h => h(App)
 });
+
+export default app;
